ef562fab43955f7feb43470fd984847ee925e341
pavel.kuznetsov.5x <pavel.kuznetsov.5x@gmail.com>
2020-06-11 18:32:57 +0300

    Changes

---
+++ app/src/main/java/com/spqrta/reusables/utility/utils/LocationUtils.kt
@@ -0,0 +1,12 @@
+package com.spqrta.reusables.utility.utils
+
+import kotlin.math.pow
+
+object LocationUtils {
+
+    fun calcVisibilityRadius(zoom: Double): Double {
+        return 1 / 2.0.pow(zoom + 1) * EQUATOR * 1000
+    }
+
+    const val EQUATOR = 40075.7
+}
